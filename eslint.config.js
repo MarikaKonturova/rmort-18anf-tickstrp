89012c69b81c62cbf57339889278170607ad14c1
MarikaKonturova <mashakonturova@gmail.com>
2024-06-26 00:18:57 +0300

    refactor: add lint-staged & commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 // @ts-check
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
-const eslintConfigPrettier = require('eslint-config-prettier');
-module.exports = tseslint.config(
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+export default tseslint.config(
   {
     // config with just ignores is the replacement for `.eslintignore`
     ignores: ['**/build/**', '**/dist/**'],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title!: 'blabla';
-  constructor() {
-    var a = 'a';
-  }
-  onMethod() {}
 }
