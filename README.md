6a338388987fa7a802a6dbacf77697216037cee2
MarikaKonturova <mashakonturova@gmail.com>
2024-06-25 23:16:11 +0300

    refactor: apply prettier and eslint

--- README.md
+++ README.md
@@ -5,11 +5,14 @@
 ![GEO82coXYAAbfzz](https://github.com/MarikaKonturova/rmort-18anf-tickstrp/assets/69147255/630a8453-ba74-401a-94f9-37bb9b13dcc6)
 
 ---
+
 ### legend:
+
 ✅️ - created config and it works
 ✅️✅️ - done
 
 ### Ambitious to-do
+
 - i18n (✅️)
 - ssr (✅️)
 - stripe
---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+module.exports = tseslint.config(
+  {
+    // config with just ignores is the replacement for `.eslintignore`
+    ignores: ['**/build/**', '**/dist/**'],
+  },
+
+  {
+    files: ['**/*.ts'],
+
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintConfigPrettier,
+    ],
+
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
