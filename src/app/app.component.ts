981755014c4a1ff92b98d04b6233d1fea290b878
MarikaKonturova <mashakonturova@gmail.com>
2024-06-25 15:33:11 +0300

    feat ngx-translate change to angular/localize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'rmort-18anf-tickstrp';
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'ru']);
-    translate.setDefaultLang('ru');
-
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang?.match(/en|ru/) ? 'ru' : 'ru');
-  }
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,34 +1,16 @@
 import {
   ApplicationConfig,
-  importProvidersFrom,
-  provideZoneChangeDetection,
+  provideZoneChangeDetection
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { routes } from './app.routes';
 
-function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-export const provideTranslation = () => ({
-  loader: {
-    provide: TranslateLoader,
-    useFactory: HttpLoaderFactory,
-    deps: [HttpClient],
-  },
-});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    importProvidersFrom([
-      HttpClientModule,
-      TranslateModule.forRoot(provideTranslation()),
-    ]),
     provideRouter(routes),
     provideClientHydration(),
   ],
