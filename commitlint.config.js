d67dc370616c56c51afe8d2d674fda7df98df9a6
MarikaKonturova <mashakonturova@gmail.com>
2024-06-26 12:28:34 +0300

    refactor: linters after review

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,22 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feature',
+        'bugfix',
+        'hotfix',
+        'refactor',
+        'revert',
+        'test',
+      ],
+    ],
+  },
+};
